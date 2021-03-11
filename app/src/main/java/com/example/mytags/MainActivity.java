6b685edc7f046445e899eedae0da7f72c7aa6b86
Mario Hotaj <43809652+PowerShot@users.noreply.github.com>
2021-03-11 20:49:35 +0100

    Interface graphique

--- app/src/main/java/com/example/mytags/MainActivity.java
+++ app/src/main/java/com/example/mytags/MainActivity.java
@@ -7,10 +7,8 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
@@ -19,11 +17,14 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,10 +32,14 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-import android.database.sqlite.SQLiteDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    // Animation
+    private Animation rotateOpen;
+    private Animation rotateClose;
+    private Animation fromBottom;
+    private Animation toBottom;
+
     //Layout
     private RecyclerView staggeredRv;
     private StaggeredRecyclerAdapter adapter;
@@ -49,16 +54,20 @@ public class MainActivity extends AppCompatActivity {
     private Dialog popupDialog;
 
     //Tag
-    String currentTag ="";
+    String tag ="issues";
 
     //Button
-    Button btnCharger;
-    Button btnPhoto;
+    private FloatingActionButton btnAddPhoto;
+    private FloatingActionButton btnAddVideo;
+    private FloatingActionButton btnAddAudio;
+    private FloatingActionButton btnAddFile;
+    private FloatingActionButton btnPlus;
+    private Boolean bBtnPlusClicked = false;
 
-    //intent id
+
+    // intent pour la gallery
     private static final int PICK_IMAGE = 100;
     static final int REQUEST_IMAGE = 101;
-
 
     Uri imageUri;
     List<Row> lst = new ArrayList<>();
@@ -66,20 +75,68 @@ public class MainActivity extends AppCompatActivity {
     //EditText
     EditText editTextTag ;
 
-    //DATABASE
-    SQLiteDatabase mediaDataBase;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Remove shadow for menu
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        //bottomNavigationView.getMenu().getItem(2).setEnabled(false);
+
         this.activity = this;
 
+        // Set Animation
+        rotateOpen = AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim);
+        rotateClose = AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim);
+        fromBottom = AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim);
+        toBottom = AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim);
 
+        // Set Button
+        btnAddPhoto = (FloatingActionButton) findViewById(R.id.BtnAddPhoto);
+        btnAddVideo = (FloatingActionButton) findViewById(R.id.BtnAddVideo);
+        btnAddAudio = (FloatingActionButton) findViewById(R.id.BtnAddAudio);
+        btnAddFile = (FloatingActionButton) findViewById(R.id.BtnAddFile);
+        btnPlus = (FloatingActionButton) findViewById(R.id.fab);
 
-        btnCharger = (Button)findViewById(R.id.buttonCharger);
+        btnAddPhoto.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               photoIntent();
+           }
+        });
+
+        btnAddVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Add video", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnAddAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Add audio", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnAddFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Add file", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAddButtonClicked();
+            }
+        });
+
+        /**
+        btnCharger = (FloatingActionButton) findViewById(R.id.BtnTakePhoto);
         btnCharger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,20 +151,52 @@ public class MainActivity extends AppCompatActivity {
                 photoIntent();
             }
         });
-
+        **/
         popupDialog = new Dialog(this);
     }
 
-    /**Intents **/
+    /** Animations **/
 
-    //GALERY INTENTS
+    private void onAddButtonClicked() {
+        setVisibility(bBtnPlusClicked);
+        setAnimation(bBtnPlusClicked);
+        bBtnPlusClicked = !bBtnPlusClicked;
+    }
+
+    private void setVisibility(Boolean clicked){
+        int visibility = (clicked ? View.INVISIBLE : View.VISIBLE);
+        btnAddPhoto.setVisibility(visibility);
+        btnAddVideo.setVisibility(visibility);
+        btnAddAudio.setVisibility(visibility);
+        btnAddFile.setVisibility(visibility);
+    }
+
+    private void setAnimation(Boolean clicked){
+        Animation animBtnPlus = (clicked ? rotateClose : rotateOpen);
+        Animation animBtnOther = (clicked ? toBottom : fromBottom);
+        // Other buttons
+        btnAddPhoto.startAnimation(animBtnOther);
+        btnAddVideo.startAnimation(animBtnOther);
+        btnAddAudio.startAnimation(animBtnOther);
+        btnAddFile.startAnimation(animBtnOther);
+
+        // Plus button
+        btnPlus.startAnimation(animBtnPlus);
+    }
+
+
+
+
+    /**LES INTENTS **/
+
+    //GALERIE INTENT
     private void galerieIntent(){
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
 
     }
 
-    //Camera Intent and URI manager
+    //INTENT CAMERA ET GESTION DES URIS DE CAMERAS
     /********************/
     String currentPhotoPath;
 
@@ -127,7 +216,7 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
-    //Intent Call and Gallery Load
+    //Appel intent chargement de la galerie
     private void photoIntent(){
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             // Create the File where the photo should go
@@ -154,28 +243,43 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    /**Return of intent management**/
+    /**GESTION DES RETOURS D'INTENT DANS MAIN**/
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
-        //Back From GALLERY
+        //Depuis intent Galerie
         if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
-
+            /*
             imageUri = data.getData();
-
-
+            lst.add(new Row(imageUri));
+            staggeredRv = findViewById(R.id.staggered_rv);
+            manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+            staggeredRv.setLayoutManager(manager);
+            adapter = new StaggeredRecyclerAdapter(MainActivity.this, lst);
+            staggeredRv.setAdapter(adapter);
+            */
             Button okButton;
             Button cancelButton;
-            //Dialog to write associate media and Tag
-            popupDialog.setContentView(R.layout.add_popup);
+            /*  popupDialog.setContentView(R.layout.add_popup);
             LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
             View view = inflater.inflate(R.layout.add_popup, null);
             cancelButton = (Button) popupDialog.findViewById(R.id.annuler_popup_button);
             okButton = (Button) popupDialog.findViewById(R.id.ajouter_image_popup_button);
-            editTextTag = (EditText) popupDialog.findViewById(R.id.textTag);
+            */
+            AlertDialog.Builder tagPopup = new AlertDialog.Builder(activity);
+            tagPopup.setTitle("Tags");
+            tagPopup.setMessage("Ajouter un tag au média");
+            tagPopup.setPositiveButton("Valider", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    //Toast.makeText(getApplicationContext(), "valeur du tag", Toast.LENGTH_SHORT ).show;
 
-            //if user clicks on cancel
+                }
+            });
+
+
+            /*
             cancelButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -183,16 +287,16 @@ public class MainActivity extends AppCompatActivity {
                     popupDialog.dismiss();
                 }
             });
-            //if user click ok => save
-            // picture Uri and Tags string
             okButton.setOnClickListener(new View.OnClickListener() {
 
                 @Override
                 public void onClick(View v) {
+                    System.out.println("set1");
+                    editTextTag = (EditText) v.findViewById(R.id.textTag);
+                    String tag2 = editTextTag.getText().toString();
 
-                    currentTag = editTextTag.getText().toString();
-
-                    if (!currentTag.isEmpty()) {
+                    System.out.println(tag2);
+                    System.out.println("set2");
                         //uri
                         lst.add(new Row(imageUri));
                         staggeredRv = findViewById(R.id.staggered_rv);
@@ -200,36 +304,36 @@ public class MainActivity extends AppCompatActivity {
                         staggeredRv.setLayoutManager(manager);
                         adapter = new StaggeredRecyclerAdapter(MainActivity.this, lst);
                         staggeredRv.setAdapter(adapter);
-                        popupDialog.dismiss();
-                        showMessage("Tag : "+currentTag+" Ajouté");
-                        //DataBase call
-                        DataBaseArch dataBaseArch = new DataBaseArch(MainActivity.this);
-                    }
-
-
-
+                    System.out.println(tag);
+                    popupDialog.dismiss();
                 }
             });
             popupDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             popupDialog.show();
-
+            */
 
         }
 
-        //Back from photo
+        //Depuis intent appareil photo
         if (requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
 
             imageUri = Uri.parse(currentPhotoPath);
-
+            /*
+            lst.add(new Row(imageUri));
+            staggeredRv = findViewById(R.id.staggered_rv);
+            manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+            staggeredRv.setLayoutManager(manager);
+            adapter = new StaggeredRecyclerAdapter(MainActivity.this, lst);
+            staggeredRv.setAdapter(adapter);
+            */
             Button okButton;
             Button cancelButton;
-            //Dialog to write associate media and Tag
             popupDialog.setContentView(R.layout.add_popup);
             LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
             View view = inflater.inflate(R.layout.add_popup, null);
             cancelButton = (Button) popupDialog.findViewById(R.id.annuler_popup_button);
             okButton = (Button) popupDialog.findViewById(R.id.ajouter_image_popup_button);
-            editTextTag = (EditText) popupDialog.findViewById(R.id.textTag);
+           editTextTag = (EditText) view.findViewById(R.id.textTag);
 
             //if user clicks on cancel
             cancelButton.setOnClickListener(new View.OnClickListener() {
@@ -246,22 +350,23 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
 
-                    currentTag = editTextTag.getText().toString();
-
-                    if (!currentTag.isEmpty()) {
-                        //uri
-                        lst.add(new Row(imageUri));
-                        staggeredRv = findViewById(R.id.staggered_rv);
-                        manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-                        staggeredRv.setLayoutManager(manager);
-                        adapter = new StaggeredRecyclerAdapter(MainActivity.this, lst);
-                        staggeredRv.setAdapter(adapter);
-                        popupDialog.dismiss();
-                        showMessage("Tag : "+currentTag+" Ajouté");
+                    tag = editTextTag.getText().toString();
+                    System.out.println(tag);
+                    if (tag.isEmpty()) {
+                        System.out.println("it works");
+                    }else{
+                        System.out.println("it doesn't works");
                     }
 
 
-
+                    //uri
+                    lst.add(new Row(imageUri));
+                    staggeredRv = findViewById(R.id.staggered_rv);
+                    manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+                    staggeredRv.setLayoutManager(manager);
+                    adapter = new StaggeredRecyclerAdapter(MainActivity.this, lst);
+                    staggeredRv.setAdapter(adapter);
+                    popupDialog.dismiss();
                 }
             });
             popupDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -269,15 +374,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-        //Get the media type
-        public String mediaType (String uri){
-            String type = uri.substring(uri.toString().length() - 3);
-            return type;
-        }
-        //Short message for successful task
-        private void showMessage (String message){
-            Toast toast = Toast.makeText(activity,message,Toast.LENGTH_SHORT);
-            toast.show();
-        }
+
 
 }
